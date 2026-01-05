b223cfdd7c891edbced16e297e06732a65354dab
tadayoedo-sizumazu <s-caddis@hera.eonet.ne.jp>
2026-01-06 01:26:42 +0900

    課題ｍｄｎ
    関数をMDNで調べて使ってみよう

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,14 @@
-const Date = () => {
-    console.log('2024年 10月 12日')
-}
-Date();
+// Dateオブジェクトを作成
+const currentDate = new Date();
+
+// 年を取得
+const year = currentDate.getFullYear();
+
+// 月を取得（0から始まるので+1する）
+const month = currentDate.getMonth() + 1;
+
+// 日を取得
+const day = currentDate.getDate();
+
+// 結果を文字列として表示
+console.log(`${year}年${month}月${day}日`);
